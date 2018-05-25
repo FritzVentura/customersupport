1bd76cf7ddfd85e09ec4a404bf99cab7eecd6391
FrederikKofoed <fk.kofoed@gmail.com>
2018-05-25 09:43:50 +0200

    JSON er lavet om til et ARRAY

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 // normal function --> hvor filnavnet for svg'en og modtagerelementet er defineret som parameters
-function jsonLoaded(svgPath,element){
+function jsonLoaded(svgPath,element,callBack){
 
     // fetch og load SVG'en til DOM'en
     fetch(svgPath)
         .then(function(response){
-            return response.text();
+            return response.json();
         })
         .then(function(data){
-            document.querySelector(element).innerHTML = data;
-            jsonLoaded();
+            
+            callBack(data);
         })
     }
     
     
-    jsonLoaded("https://kea-alt-del.dk/customersupport/?count=10", "#template_modtager", function(){
-        document.querySelector("#bike").style.display = "none";
-        console.log("svg2 er loaded");
-    });
+    jsonLoaded("https://kea-alt-del.dk/customersupport/?count=10", "#template_modtager", showData);
+
+
+    function showData(data){
+        //  document.querySelector("#bike").style.display = "none";
+          console.log("json er loaded", data);
+      }
